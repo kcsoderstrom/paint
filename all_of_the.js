28202bea54a1fb343d26af7b7db258def283d5ae
App Academy Student <appacademy@Apps-Mac-mini-22.local>
2014-11-08 17:03:50 -0500

    Add sloths as preloaded on-canvas images

--- all_of_the.js
+++ all_of_the.js
@@ -1,14 +1,34 @@
 var SlothCanvas = function(canvasEl) {
-  this.ctx = canvasEl.getContext();
+  this.ctx = canvasEl.getContext("2d");
+  this.selected_sloth_src = null;
 };
 
 SlothCanvas.prototype.start = function() {
-  // who the fuck knows what's happening here
+  var mouse_is_down = false;
   var ctx = this.ctx;
-  $("#drawing-canvas").on("mousedown", function(event){
-    var x = event.pageX;
-    var y = event.pageY;
+  var that = this;
+  $(window).on("mousedown", function(event){
+    mouse_is_down = true;
+  });
 
-    console.log("the mouse is down gurl");
-  })
+  $(window).on("mouseup", function(event){
+    mouse_is_down = false;
+  });
+
+  $("#drawing-canvas").on("mousemove", function(event){
+    if(mouse_is_down) {
+      var x = event.pageX - 10;
+      var y = event.pageY - 110;
+      console.log(x);
+
+      ctx.fillStyle = "rgba(0,0,0,1)";
+      ctx.fillRect(x,y,10,10);
+    }
+  });
+
+  $("img#01").on("click", function() {
+    that.selected_sloth_src = getElementById("01");
+  });
+
+
 };
